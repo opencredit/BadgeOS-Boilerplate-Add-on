55d2ae75ab1a772a1f1b3905c3e54211af55905b
Brian Richards <brian@rzen.net>
2013-06-28 11:15:43 -0400

    first commit

---
+++ badgeos-add-on.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Plugin Name: BadgeOS Add-On Boilerplate
+ * Plugin URI: http://www.learningtimes.com/
+ * Description: This BadgeOS add-on automatically...
+ * Author: Credly
+ * Version: 1.0.0
+ * Author URI: https://credly.com/
+ * License: GNU AGPL
+ */
+
+/**
+ * Our main plugin instantiation class
+ *
+ * This contains important things that our relevant to
+ * our add-on running correctly. Things like registering
+ * custom post types, taxonomies, posts-to-posts
+ * relationships, and the like.
+ *
+ * @since 1.0.0
+ */
+class BadgeOS_Addon {
+
+	/**
+	 * Get everything running.
+	 *
+	 * @since 1.0.0
+	 */
+	function __construct() {
+
+		// Define plugin constants
+		$this->basename       = plugin_basename( __FILE__ );
+		$this->directory_path = plugin_dir_path( __FILE__ );
+		$this->directory_url  = plugins_url( dirname( $this->basename ) );
+
+		// Load translations
+		load_plugin_textdomain( 'badgeos-addon', false, dirname( $this->basename ) . '/languages' );
+
+		// Run our activation and deactivation hooks
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_activation_hook( __FILE__, array( $this, 'deactivate' ) );
+
+		// If BadgeOS is unavailable, deactivate our plugin
+		add_action( 'admin_notices', array( $this, 'maybe_disable_plugin' ) );
+
+		// Include our other plugin files
+		add_action( 'init', array( $this, 'includes' ) );
+
+	} /* __construct() */
+
+
+	/**
+	 * Include our plugin dependencies
+	 *
+	 * @since 1.0.0
+	 */
+	public function includes() {
+
+		// If BadgeOS is available...
+		if ( $this->meets_requirements() ) {
+
+			// Include some files
+
+		}
+
+	} /* includes() */
+
+	/**
+	 * Activation hook for the plugin.
+	 *
+	 * @since 1.0.0
+	 */
+	public function activate() {
+
+		// If BadgeOS is available, run our activation functions
+		if ( $this->meets_requirements() ) {
+
+			// Do some activation things
+
+		}
+
+	} /* activate() */
+
+	/**
+	 * Deactivation hook for the plugin.
+	 *
+	 * Note: this plugin may auto-deactivate due
+	 * to $this->maybe_disable_plugin()
+	 *
+	 * @since 1.0.0
+	 */
+	public function deactivate() {
+
+		// Do some deactivation things.
+
+	} /* deactivate() */
+
+	/**
+	 * Check if BadgeOS is available
+	 *
+	 * @since  1.0.0
+	 * @return bool True if BadgeOS is available, false otherwise
+	 */
+	public static function meets_requirements() {
+
+		if ( class_exists('BadgeOS') )
+			return true;
+		else
+			return false;
+
+	} /* meets_requirements() */
+
+	/**
+	 * Potentially output a custom error message and deactivate
+	 * this plugin, if we don't meet requriements.
+	 *
+	 * This fires on admin_notices.
+	 *
+	 * @since 1.0.0
+	 */
+	public function maybe_disable_plugin() {
+
+		if ( ! $this->meets_requirements() ) {
+			// Display our error
+			echo '<div id="message" class="error">';
+			echo '<p>' . sprintf( __( 'BadgeOS Add-On requires BadgeOS and has been <a href="%s">deactivated</a>. Please install and activate BadgeOS and then reactivate this plugin.', 'badgeos-addon' ), admin_url( 'plugins.php' ) ) . '</p>';
+			echo '</div>';
+
+			// Deactivate our plugin
+			deactivate_plugins( $this->basename );
+		}
+
+	} /* maybe_disable_plugin() */
+
+} /* BadgeOS_Addon */
+
+// Instantiate our class to a global variable that we can access elsewhere
+$GLOBALS['badgeos_addon'] = new BadgeOS_Addon();
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== BadgeOS Add-on ===
+Contributors: credly, webdevstudios
+Donate link: http://badgeos.org/contribute/donate/
+Tags: badge, badges, openbadges, credly, OBI, mozilla, open badges, achievement, award, reward, engagement, submission, nomination, API, open credit, credit, badgeOS
+Requires at least: 3.5
+Tested up to: 3.5.2
+Stable tag: 1.0.0
+License: GNU AGPLv3
+License URI: http://www.gnu.org/licenses/agpl-3.0.html
+
+This add-on to BadgeOS does some pretty great things...
+
+== Description ==
+
+This add-on to [BadgeOS plugin](http://wordpress.org/extend/plugins/badgeos/ "BadgeOS") automatically ...
+
+[Get the BadgeOS plugin](http://wordpress.org/extend/plugins/badgeos/ "BadgeOS").
+
+= Resources / Helpful Links =
+
+BadgeOS Add-On is made available by [Credly, LLC](https://credly.com/ "Credly web site"). Here are some ways to stay connected and to see what else we are up to:
+
+* [BadgeOS.org](http://badgeos.org/ "BadgeOS web site") - Contact Us, Video Tutorials, Examples, News
+* [Credly.com](https://credly.com/ "Credly web site") - Recognize, manage and share lifelong achievement
+* [BadgeOS on GitHub](https://github.com/opencredit/badgeos "BadgeOS on GitHub") - Report issues, contribute code
+
+== Installation ==
+
+
+1. First get and activate the free [BadgeOS plugin](http://wordpress.org/extend/plugins/badgeos/ "BadgeOS").
+2. Then upload the 'badgeos-add-on' plugin via the 'Plugins' menu in WordPress (or upload to the '/wp-content/plugins/' directory).
+3. ...
+
+== Frequently Asked Questions ==
+
+...
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Intial release
+
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* Intial release
